bb2a6624cc1724fd40ba3cb69bc80778229e37b8
«Evgeny» <mushnikovz@mail.ru>
2023-09-08 13:39:19 +0300

    Добавил popup по аферте

--- src/components/popup/popup.js
+++ src/components/popup/popup.js
@@ -1,6 +1,9 @@
 const body = document.querySelector('body')
 
 export function openModal(id) {
+  document.querySelectorAll('[data-popup-target]').forEach(popup => {
+    if(popup.classList.contains('show')) { popup.classList.remove('show'); }
+  });
   document.querySelector(`[data-popup-target="${id}"]`).classList.add('show');
   body.classList.add('hidden');
 }
